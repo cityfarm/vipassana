d9691d3694ffdd85e3b41ee161ce2ad4f7d07619
cityfarm <cityfarm19@gmail.com>
2024-10-31 10:16:16 +0700

    'ok'

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo50.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <div class="container">
+          <h1>Vipassana Việt Nam</h1>
+          <p>Tổ chức các khóa thiền Vipassana do thiền sư S.N. Goenka giảng dạy</p>
+          <p>theo truyền thống của Ngài Sayagyi U Ba Khin.</p>
+          <p><a href="https://www.dhamma.org/vi/schedules/noncenter/vn" target="_blank"><strong>Lịch các khóa thiền:</strong></a></p>
+          <p><a href="https://www.dhamma.org/vi/schedules/noncenter/vn" target="_blank">https://www.dhamma.org/vi/schedules/noncenter/vn</a></p>
+        </div>
+
+      </header >
+    </div >
+  );
+}
+
+export default App;
