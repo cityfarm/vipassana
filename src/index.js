422b60a5464249f3d9cb682a00c96c10dd11c3c3
cityfarm <cityfarm19@gmail.com>
2024-10-31 20:11:53 +0700

    'ok'

--- src/index.js
+++ src/index.js
@@ -7,24 +7,13 @@ import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-try {
-  root.render(
-      <React.StrictMode>
-          <App />
-      </React.StrictMode>
-  );
-} catch (error) {
-  console.error("Không tải được App.js:", error);
-  root.render(
-      <div style={{ textAlign: 'center', padding: '20px' }}>
-          <h1>Vipassana Việt Nam</h1>
-          <p>Tổ chức các khóa thiền Vipassana do thiền sư S.N. Goenka giảng dạy</p>
-          <p>theo truyền thống của Ngài Sayagyi U Ba Khin.</p>
-          <p><a href="https://www.dhamma.org/vi/schedules/noncenter/vn" target="_blank"><strong>Lịch các khóa thiền:</strong></a></p>
-          <p><a href="https://www.dhamma.org/vi/schedules/noncenter/vn" target="_blank">https://www.dhamma.org/vi/schedules/noncenter/vn</a></p>
-          <p>(Đây là trang web dự phòng khi tải trang bị lỗi!)</p>
-      </div>
-  );
-}
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+)
+
 
 reportWebVitals();
